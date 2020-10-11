99450e3d1553e975b0180155631e7cc0cd782a79
Michael Mersiades <fakename014@gmail.com>
2020-10-11 17:42:23 +1100

    Converts MenuPage to use Grommet grid system

--- src/components/MenuPage.tsx
+++ src/components/MenuPage.tsx
@@ -2,7 +2,6 @@ import React, { FC, useState } from 'react';
 import { Button, Box, Image, Heading, FormField, TextInput, Text, Grid, BoxProps } from 'grommet';
 import { Close } from 'grommet-icons';
 import { useAuth } from '../contexts/authContext';
-import styled from 'styled-components';
 import '../assets/styles/transitions.css';
 import CreateGameForm from './CreateGameForm';
 import { useDiscordUser } from '../contexts/discordUserContext';
@@ -16,25 +15,6 @@ const background = {
   size: 'contain',
   image: 'url(/images/cover-background.jpg)',
 };
-
-//
-const ButtonsContainer = styled(Box as React.FC<BoxProps & JSX.IntrinsicElements['div']>)`
-  position: absolute;
-  bottom: calc(4vh + 2px);
-  min-width: 172px;
-  width: 25vw;
-  max-width: 300px;
-  left: 15vw;
-`;
-
-const TitleContainer = styled.div`
-  position: absolute;
-  bottom: 4vh;
-  min-width: 172px;
-  width: 25vw;
-  max-width: 300px;
-  right: 15vw;
-`;
 
 const MenuPage: FC = () => {
   const [buttonsContainer, setButtonsContainer] = useState(0);
@@ -56,11 +36,41 @@ const MenuPage: FC = () => {
 
   return (
     <Box fill background={background}>
-      <Text margin="medium" size="medium">
-        {username && `Welcome, ${username}`}
-      </Text>
-        {buttonsContainer === 0 && (
-          <ButtonsContainer animation={{ type: "slideUp", size: "large", duration: 750}}>
+    <Grid
+      rows={["1/2", "1/2"]}
+      columns={["15%", "auto", "15%"]}
+      gap={{ column: "xsmall", row: "none"}}
+      fill
+      responsive
+      areas={[
+        { name: "leftMargin", start: [0,0], end: [0, 1]},
+        { name: "centerTop", start: [1,0], end: [1, 0]},
+        { name: "centerBottom", start: [1,1], end: [1, 1]},
+        { name: "rightMargin", start: [2,0], end: [2, 1]}
+
+      ]}
+      >
+      <Box gridArea="leftMargin"/>
+      <Box gridArea="centerTop">
+           <Heading level="4">
+         {username && `Welcome, ${username}`}
+         </Heading>
+      </Box>
+      <Box gridArea="centerBottom">
+        <Grid
+          rows={["full"]}
+          columns={[["250px", "450px"], ["10px", "auto"], ["250px", "450px"]]}
+          fill
+          justifyContent="between"
+          areas={[
+            { name: "buttonsContainer", start: [0,0], end: [0, 0]},
+            { name: "spacer", start: [1,0], end: [1, 0]},
+            { name: "titleContainer", start: [2,0], end: [2, 0]},
+          ]}
+        >
+        <Box gridArea="buttonsContainer" alignSelf="end">
+               {buttonsContainer === 0 && (
+          <Box animation={{ type: "slideUp", size: "large", duration: 750}}>
             <Box gap="small">
               {!!userData && userData.userByDiscordId.gameRoles.length > 0 && (
                 <Button
@@ -90,10 +100,10 @@ const MenuPage: FC = () => {
               />
              <Button label="LOG OUT" size="large" alignSelf="center" fill onClick={() => handleLogout()} />
             </Box>
-          </ButtonsContainer>
+          </Box>
         )}
-        {buttonsContainer === 1 && (
-          <ButtonsContainer animation={{ type: "slideUp", size: "large", duration: 750}}>
+         { buttonsContainer === 1 && (
+          <Box animation={{ type: "slideUp", size: "large", duration: 750}}>
             <Grid
               rows={['xsmall']}
               columns={['xxsmall', 'small']}
@@ -114,10 +124,10 @@ const MenuPage: FC = () => {
               </Box>
             </Grid>
             <GamesList gameRoles={userData.userByDiscordId.gameRoles} />
-          </ButtonsContainer>
+          </Box>
         )}
         {buttonsContainer === 2 && (
-          <ButtonsContainer animation={{ type: "slideUp", size: "large", duration: 750}}>
+          <Box animation={{ type: "slideUp", size: "large", duration: 750}}>
             <Grid
               rows={['xsmall']}
               columns={['xxsmall', 'small']}
@@ -146,10 +156,10 @@ const MenuPage: FC = () => {
               </Text>
               <Button label="SUBMIT" primary size="large" alignSelf="center" fill />
             </Box>
-          </ButtonsContainer>
+          </Box>
         )}
         {buttonsContainer === 3 && (
-          <ButtonsContainer animation={{ type: "slideUp", size: "large", duration: 750}}>
+          <Box animation={{ type: "slideUp", size: "large", duration: 750}}>
             <Grid
               rows={['xsmall']}
               columns={['xxsmall', 'small']}
@@ -170,44 +180,26 @@ const MenuPage: FC = () => {
               </Box>
             </Grid>
             <CreateGameForm />
-          </ButtonsContainer>
+          </Box>
         )}
-        {buttonsContainer === 3 && (
-          <ButtonsContainer animation={{ type: "slideUp", size: "large", duration: 750}}>
-            <Grid
-              rows={['xsmall']}
-              columns={['xxsmall', 'small']}
-              justifyContent="between"
-              align="center"
-              areas={[
-                { name: 'header-left', start: [0, 0], end: [0, 0] },
-                { name: 'header-right', start: [1, 0], end: [1, 0] },
-              ]}
-            >
-              <Box gridArea="header-left" align="start" alignContent="center">
-                <Close color="accent-1" onClick={() => setButtonsContainer(0)} />
-              </Box>
-              <Box gridArea="header-right">
-                <Heading level={1} margin={{ vertical: 'small' }} size="small" textAlign="end">
-                  CREATE GAME
-                </Heading>
-              </Box>
-            </Grid>
-            <CreateGameForm />
-          </ButtonsContainer>
-        )}
-      <TitleContainer>
-        <Box>
-          <Image
+        </Box>
+        <Box gridArea="spacer" alignSelf="end"/> 
+        <Box gridArea="titleContainer" alignSelf="end">
+         <Box>
+           <Image
             fit="contain"
             fill={true}
             src="images/cover-title.png"
             alt="D. Vincent Baker & Meguey Baker Apocalypse World"
           />
         </Box>
-      </TitleContainer>
+        </Box>
+        </Grid>
+      </Box>
+      <Box gridArea="rightMargin"/>
+    </Grid>
     </Box>
-  );
+  )
 };
 
 export default MenuPage;
