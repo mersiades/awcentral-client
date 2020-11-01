5dd0351c4b379cbf5b0b67f19eadb137f01c9013
Michael Mersiades <fakename014@gmail.com>
2020-11-01 18:45:06 +1100

    Adds remaining generic moves to MovesPanel

--- src/components/MovesPanel.tsx
+++ src/components/MovesPanel.tsx
@@ -38,6 +38,9 @@ const LevelOneAccordionLabel: FC<LevelOneAccordionLabelProps> = ({moveKind}) => 
 const MovesPanel: FC<MovesPanelProps> = ({allMoves, closePanel}) => {
 
   const basicMoves = allMoves.filter((move) => move.kind === MoveKinds.basic)
+  const peripheralMoves = allMoves.filter((move) => move.kind === MoveKinds.peripheral)
+  const battleMoves = allMoves.filter((move) => move.kind === MoveKinds.battle)
+  const roadWarMoves = allMoves.filter((move) => move.kind === MoveKinds.roadWar)
 
   
   return (
@@ -50,6 +53,42 @@ const MovesPanel: FC<MovesPanelProps> = ({allMoves, closePanel}) => {
           <AccordionPanel label={<LevelOneAccordionLabel moveKind={MoveKinds.basic}/>}>
             <Accordion >
             {basicMoves.map((move) => (
+                <AccordionPanel label={move.name}>
+                  <Box pad="12px">
+                    <ReactMarkdown>{move.description}</ReactMarkdown>
+                  </Box>
+                </AccordionPanel>
+
+            ))}
+            </Accordion>
+          </AccordionPanel>
+          <AccordionPanel label={<LevelOneAccordionLabel moveKind={MoveKinds.peripheral}/>}>
+            <Accordion >
+            {peripheralMoves.map((move) => (
+                <AccordionPanel label={move.name}>
+                  <Box pad="12px">
+                    <ReactMarkdown>{move.description}</ReactMarkdown>
+                  </Box>
+                </AccordionPanel>
+
+            ))}
+            </Accordion>
+          </AccordionPanel>
+          <AccordionPanel label={<LevelOneAccordionLabel moveKind={MoveKinds.battle}/>}>
+            <Accordion >
+            {battleMoves.map((move) => (
+                <AccordionPanel label={move.name}>
+                  <Box pad="12px">
+                    <ReactMarkdown>{move.description}</ReactMarkdown>
+                  </Box>
+                </AccordionPanel>
+
+            ))}
+            </Accordion>
+          </AccordionPanel>
+          <AccordionPanel label={<LevelOneAccordionLabel moveKind={MoveKinds.roadWar}/>}>
+            <Accordion >
+            {roadWarMoves.map((move) => (
                 <AccordionPanel label={move.name}>
                   <Box pad="12px">
                     <ReactMarkdown>{move.description}</ReactMarkdown>
