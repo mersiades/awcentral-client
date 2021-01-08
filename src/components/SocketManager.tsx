48dc0bd08db98d63a9be8249f80287d770a7118c
Michael Mersiades <fakename014@gmail.com>
2021-01-08 20:51:38 +1100

    Deletes getAuthHeader() and appendChannels mutation

--- src/components/SocketManager.tsx
+++ src/components/SocketManager.tsx
@@ -6,13 +6,10 @@ import SockJS from 'sockjs-client';
 import { GameResponse } from '../@types';
 import { Roles, WebsocketResponses } from '../@types/enums';
 import { WebsocketContext } from '../contexts/websocketContext';
-import APPEND_CHANNELS, { AppendChannelsData, AppendChannelsVars } from '../mutations/appendChannels';
 // import USER_BY_DISCORD_ID from '../queries/userByDiscordId';
 
 const SocketManager: FC = ({ children }) => {
   const [stompClient, setStompClient] = useState<Client | undefined>();
-
-  // const [appendChannels] = useMutation<AppendChannelsData, AppendChannelsVars>(APPEND_CHANNELS);
   // const history = useHistory();
 
   const handleGame = () => console.log('mock handling game');
@@ -22,21 +19,14 @@ const SocketManager: FC = ({ children }) => {
   //     console.log('Incoming game websocket message:', type);
   //     switch (type) {
   //       case WebsocketResponses.addChannels:
-  //         if (!!id && !!textChannelId && !!voiceChannelId) {
-  //           await appendChannels({
-  //             variables: { id, textChannelId, voiceChannelId },
-  //             // refetchQueries: [{ query: USER_BY_DISCORD_ID, variables: { discordId } }],
-  //           });
-  //         } else {
-  //           console.warn('Unable to append channels: missing mutation variable');
-  //         }
+  //
   //         history.push(`/game/${textChannelId}`, { role: Roles.mc });
   //         break;
   //       default:
   //         break;
   //     }
   //   },
-  //   [appendChannels, history]
+  //   [ history]
   // );
 
   // const connectStompClient = useCallback(() => {
