ffe358c7758822b746029102b2b2020e76326421
Michael Mersiades <fakename014@gmail.com>
2021-01-09 18:33:22 +1100

    Adds progress boxes for character creation

--- src/@types/dataInterfaces.ts
+++ src/@types/dataInterfaces.ts
@@ -10,100 +10,100 @@ import { CharacterMove, Look, Move } from './staticDataInterfaces';
 
 // -------------------------------------------------- User interfaces -------------------------------------------------- //
 export interface User {
-  id: string
-  userId: string
-  gameRoles: GameRole[]
+  id: string;
+  userId: string;
+  gameRoles: GameRole[];
 }
 
 // -------------------------------------------------- Game interfaces -------------------------------------------------- //
 export interface Game {
   id: string;
   name: string;
-  commsApp: string
-  commsUrl: string
-  hasFinishedPreGame: boolean
-  mc: { displayName: string, id: string }
-  players: { displayName: string, id: string }[]
-  gameRoles: GameRole[]
-  invitees: string[]
+  commsApp: string;
+  commsUrl: string;
+  hasFinishedPreGame: boolean;
+  mc: { displayName: string; id: string };
+  players: { displayName: string; id: string }[];
+  gameRoles: GameRole[];
+  invitees: string[];
 }
 
 export interface GameRole {
   id: string;
   role: Roles;
-  userId: string
-  characters: Character[] 
-  npcs: Npc[] 
-  threats: Threat[]
+  userId: string;
+  characters: Character[];
+  npcs: Npc[];
+  threats: Threat[];
   game?: Game;
 }
 // ------------------------------------------------- Player interfaces ------------------------------------------------- //
 
 export interface Character {
-  id: string
-  playbook: PlayBooks
-  statsBlock: CharacterStat[]
-  hxBlock: HxStat[]
-  gear: string[]
-  looks: Look[]
-  name?: string
-  playbookUnique?: PlaybookUnique
-  characterMoves?: CharacterMove[]
+  id: string;
+  playbook: PlayBooks;
+  statsBlock: CharacterStat[];
+  hxBlock: HxStat[];
+  gear: string[];
+  looks: Look[];
+  characterMoves: CharacterMove[];
+  name?: string;
+  playbookUnique?: PlaybookUnique;
 }
 
 export interface CharacterStat {
-  id: string
-  stat: Stats
-  value: number
-  isHighlighted: Boolean
+  id: string;
+  stat: Stats;
+  value: number;
+  isHighlighted: Boolean;
 }
 
 export interface HxStat {
-  characterId: string
-  characterName: string
-  hxValue: number
+  characterId: string;
+  characterName: string;
+  hxValue: number;
 }
 
 export interface PlaybookUnique {
-  id: string
-  type: UniqueTypes
-  brainerGear?: BrainerGear
-  angelKit?: AngelKit
-  customWeapons?: CustomWeapons
+  id: string;
+  type: UniqueTypes;
+  brainerGear?: BrainerGear;
+  angelKit?: AngelKit;
+  customWeapons?: CustomWeapons;
 }
 
 export interface BrainerGear {
-  id: string
-  brainerGear: string[]
+  id: string;
+  brainerGear: string[];
 }
 
 export interface AngelKit {
-  id: string
-  description: string
-  stock: number
-  angelKitMoves: Move[]
-  hasSupplier: boolean
-  supplierText: string
+  id: string;
+  description: string;
+  stock: number;
+  angelKitMoves: Move[];
+  hasSupplier: boolean;
+  supplierText: string;
 }
 
 export interface CustomWeapons {
-  id: string
-  weapons: string[]
+  id: string;
+  weapons: string[];
 }
 
 // --------------------------------------------------- MC interfaces --------------------------------------------------- //
 
 export interface Npc {
-  id: string
-  name: string
-  description?: string
+  id: string;
+  name: string;
+  description?: string;
 }
 
 export interface Threat {
-  id: string
-  name: string
-  threatKind: Threats
-  impulse: string
-  description?: string
-  stakes?: string
+  id: string;
+  name: string;
+  threatKind: Threats;
+  impulse: string;
+  description?: string;
+  stakes?: string;
 }
--- src/pages/PreGamePage.tsx
+++ src/pages/PreGamePage.tsx
@@ -1,11 +1,17 @@
-import { useQuery } from '@apollo/client';
-import { Box } from 'grommet';
-import { set } from 'lodash';
 import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
 import { useHistory, useParams } from 'react-router-dom';
+
+import { Box } from 'grommet';
+import CloseButton from '../components/CloseButton';
+import { HeadingWS, RedBox, TextWS } from '../config/grommetConfig';
+import GAME, { GameData, GameVars } from '../queries/game';
 import { useGameRoles } from '../contexts/gameRoleContext';
 import { useKeycloakUser } from '../contexts/keycloakUserContext';
-import GAME, { GameData, GameVars } from '../queries/game';
+import { Character } from '../@types/dataInterfaces';
+import { Roles } from '../@types/enums';
+import { decapitalize } from '../helpers/decapitalize';
+import { Checkbox, Checkmark } from 'grommet-icons';
 
 const PreGamePage = () => {
   // -------------------------------------------------- Component state ---------------------------------------------------- //
@@ -18,21 +24,109 @@ const PreGamePage = () => {
   const { id: userId } = useKeycloakUser();
   const { userGameRole, mcGameRole, allPlayerGameRoles, otherPlayerGameRoles, setGameRoles } = useGameRoles();
 
+  // ----------------------------------------- Component functions and variables ------------------------------------------- //
+
+  const pathToGame = userGameRole?.role === Roles.mc ? `/mc-game/${game?.id}` : `/player-game/${game?.id}`;
+
   // ------------------------------------------------------ Effects -------------------------------------------------------- //
 
-  // useEffect(() => {
-  //   game?.hasFinishedPreGame && history.push('')
-  // }, [game])
+  // Send User to MenuPage if not a member of this game
+  useEffect(() => {
+    if (!!game && !!userId) {
+      const memberIds = game?.gameRoles.map((gameRole) => gameRole.userId);
+      if (!memberIds.includes(userId)) {
+        history.push('/menu');
+      }
+    }
+  }, [game, userId, history]);
 
+  // Send User to MCPage or PlayerPage if pre-game is already complete
+  useEffect(() => {
+    if (!!userGameRole && !!game && game?.hasFinishedPreGame) {
+      history.push(pathToGame);
+    }
+  }, [game, userGameRole, pathToGame, history]);
+
+  // Set the Game's GameRoles
   useEffect(() => {
     if (!!game && game.gameRoles.length > 0 && !!userId && !!setGameRoles) {
       setGameRoles(game.gameRoles, userId);
     }
   }, [game, userId, setGameRoles]);
 
+  // ------------------------------------------------------ Render -------------------------------------------------------- //
+
+  const renderPlayerBox = (character: Character, index: number) => {
+    console.log('character', character);
+    return (
+      <RedBox key={!!character?.id ? character.id : index} align="center" pad={{ horizontal: '12px' }}>
+        <HeadingWS level="3" margin={{ vertical: '12px' }}>
+          {`${!!character?.name ? character.name : 'Name'}  --  ${
+            !!character?.playbook ? decapitalize(character.playbook) : 'Playbook'
+          }`}
+        </HeadingWS>
+
+        <Box direction="row" align="center" justify="around">
+          <Box align="center" margin="12px" gap="12px">
+            {!!character && character.looks.length === 5 ? (
+              <Checkmark size="xlarge" color="accent-1" />
+            ) : (
+              <Checkbox size="xlarge" color="neutral-1" />
+            )}
+            <TextWS size="large">Looks</TextWS>
+          </Box>
+          <Box align="center" pad="12px" gap="12px">
+            {!!character && character.statsBlock.length === 5 ? (
+              <Checkmark size="xlarge" color="accent-1" />
+            ) : (
+              <Checkbox size="xlarge" color="neutral-1" />
+            )}
+            <TextWS size="large">Stats</TextWS>
+          </Box>
+          <Box align="center" pad="12px" gap="12px">
+            {!!character && character.gear.length > 0 ? (
+              <Checkmark size="xlarge" color="accent-1" />
+            ) : (
+              <Checkbox size="xlarge" color="neutral-1" />
+            )}
+            <TextWS size="large">Gear</TextWS>
+          </Box>
+          <Box align="center" pad="12px" gap="12px">
+            {!!character && !!character.playbookUnique ? (
+              <Checkmark size="xlarge" color="accent-1" />
+            ) : (
+              <Checkbox size="xlarge" color="neutral-1" />
+            )}
+            <TextWS size="large">Unique</TextWS>
+          </Box>
+          <Box align="center" pad="12px" gap="12px">
+            {!!character && character.characterMoves.length > 2 ? (
+              <Checkmark size="xlarge" color="accent-1" />
+            ) : (
+              <Checkbox size="xlarge" color="neutral-1" />
+            )}
+            <TextWS size="large">Moves</TextWS>
+          </Box>
+          <Box align="center" pad="12px" gap="12px">
+            {!!character && character.hxBlock.length > 0 ? (
+              <Checkmark size="xlarge" color="accent-1" />
+            ) : (
+              <Checkbox size="xlarge" color="neutral-1" />
+            )}
+            <TextWS size="large">Hx</TextWS>
+          </Box>
+        </Box>
+      </RedBox>
+    );
+  };
+
   return (
-    <Box background="black" fill align="center" justify="center">
-      {gameId}
+    <Box background="black" fill align="center" justify="start" pad="24px">
+      <CloseButton handleClose={() => history.push(pathToGame)} />
+      <HeadingWS level="2">PRE-GAME PROGRESS</HeadingWS>
+      {allPlayerGameRoles?.map((gameRole, index) => {
+        return renderPlayerBox(gameRole.characters[0], index);
+      })}
     </Box>
   );
 };
