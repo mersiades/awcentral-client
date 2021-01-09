bfc5cc80f2f24fe66b81bf413eb26e2bbc8ead2d
Michael Mersiades <fakename014@gmail.com>
2021-01-09 20:00:10 +1100

    Adds mc instructions to PreGamePage

--- src/pages/PreGamePage.tsx
+++ src/pages/PreGamePage.tsx
@@ -18,11 +18,11 @@ const PreGamePage = () => {
   // -------------------------------------------------- 3rd party hooks ---------------------------------------------------- //
   const { gameId } = useParams<{ gameId: string }>();
   const history = useHistory();
-  const { data: gameData } = useQuery<GameData, GameVars>(GAME, { variables: { gameId } });
+  const { data: gameData } = useQuery<GameData, GameVars>(GAME, { variables: { gameId } /*pollInterval: 2500*/ });
   const game = gameData?.game;
   // ------------------------------------------------------- Hooks --------------------------------------------------------- //
   const { id: userId } = useKeycloakUser();
-  const { userGameRole, mcGameRole, allPlayerGameRoles, otherPlayerGameRoles, setGameRoles } = useGameRoles();
+  const { userGameRole, allPlayerGameRoles, setGameRoles } = useGameRoles();
 
   // ----------------------------------------- Component functions and variables ------------------------------------------- //
 
@@ -59,59 +59,64 @@ const PreGamePage = () => {
   const renderPlayerBox = (character: Character, index: number) => {
     console.log('character', character);
     return (
-      <RedBox key={!!character?.id ? character.id : index} align="center" pad={{ horizontal: '12px' }}>
-        <HeadingWS level="3" margin={{ vertical: '12px' }}>
+      <RedBox
+        key={!!character?.id ? character.id : index}
+        align="center"
+        pad={{ top: '18px', horizontal: '12px' }}
+        margin="12px"
+      >
+        <HeadingWS level="3" margin={{ vertical: '0' }}>
           {`${!!character?.name ? character.name : 'Name'}  --  ${
             !!character?.playbook ? decapitalize(character.playbook) : 'Playbook'
           }`}
         </HeadingWS>
 
         <Box direction="row" align="center" justify="around">
-          <Box align="center" margin="12px" gap="12px">
+          <Box align="center" margin={{ horizontal: '6px' }} gap="12px">
             {!!character && character.looks.length === 5 ? (
-              <Checkmark size="xlarge" color="accent-1" />
+              <Checkmark size="large" color="accent-1" />
             ) : (
-              <Checkbox size="xlarge" color="neutral-1" />
+              <Checkbox size="large" color="neutral-1" />
             )}
             <TextWS size="large">Looks</TextWS>
           </Box>
           <Box align="center" pad="12px" gap="12px">
             {!!character && character.statsBlock.length === 5 ? (
-              <Checkmark size="xlarge" color="accent-1" />
+              <Checkmark size="large" color="accent-1" />
             ) : (
-              <Checkbox size="xlarge" color="neutral-1" />
+              <Checkbox size="large" color="neutral-1" />
             )}
             <TextWS size="large">Stats</TextWS>
           </Box>
           <Box align="center" pad="12px" gap="12px">
             {!!character && character.gear.length > 0 ? (
-              <Checkmark size="xlarge" color="accent-1" />
+              <Checkmark size="large" color="accent-1" />
             ) : (
-              <Checkbox size="xlarge" color="neutral-1" />
+              <Checkbox size="large" color="neutral-1" />
             )}
             <TextWS size="large">Gear</TextWS>
           </Box>
           <Box align="center" pad="12px" gap="12px">
             {!!character && !!character.playbookUnique ? (
-              <Checkmark size="xlarge" color="accent-1" />
+              <Checkmark size="large" color="accent-1" />
             ) : (
-              <Checkbox size="xlarge" color="neutral-1" />
+              <Checkbox size="large" color="neutral-1" />
             )}
             <TextWS size="large">Unique</TextWS>
           </Box>
           <Box align="center" pad="12px" gap="12px">
             {!!character && character.characterMoves.length > 2 ? (
-              <Checkmark size="xlarge" color="accent-1" />
+              <Checkmark size="large" color="accent-1" />
             ) : (
-              <Checkbox size="xlarge" color="neutral-1" />
+              <Checkbox size="large" color="neutral-1" />
             )}
             <TextWS size="large">Moves</TextWS>
           </Box>
           <Box align="center" pad="12px" gap="12px">
             {!!character && character.hxBlock.length > 0 ? (
-              <Checkmark size="xlarge" color="accent-1" />
+              <Checkmark size="large" color="accent-1" />
             ) : (
-              <Checkbox size="xlarge" color="neutral-1" />
+              <Checkbox size="large" color="neutral-1" />
             )}
             <TextWS size="large">Hx</TextWS>
           </Box>
@@ -121,12 +126,30 @@ const PreGamePage = () => {
   };
 
   return (
-    <Box background="black" fill align="center" justify="start" pad="24px">
+    <Box background="black" fill align="center" justify="start" pad="24px" overflow="auto">
       <CloseButton handleClose={() => history.push(pathToGame)} />
       <HeadingWS level="2">PRE-GAME PROGRESS</HeadingWS>
-      {allPlayerGameRoles?.map((gameRole, index) => {
-        return renderPlayerBox(gameRole.characters[0], index);
-      })}
+      {userGameRole?.role === Roles.mc && (
+        <Box flex="grow" style={{ maxWidth: '812px' }} gap="3px">
+          <TextWS>Use this time to build the world you will play in.</TextWS>
+          <TextWS>Ask your players lots of questions about their characters and the world.</TextWS>
+          <TextWS>While the players are making their characters, here are some things to get out up-front:</TextWS>
+          <ul>
+            <li>Your characters don't have to be friends, but they should definitely be allies.</li>
+            <li>Your characters are unique in Apocalypse World.</li>
+            <li>1-armor can be armor or clothing. 2-armor is armor.</li>
+            <li>
+              Is <em>barter</em> a medium of exchange? What do you use?
+            </li>
+            <li>I'm not out to get you. I'm here to find out what's going to happen. Same as you!</li>
+          </ul>
+        </Box>
+      )}
+      <Box direction="row" wrap justify="center">
+        {allPlayerGameRoles?.map((gameRole, index) => {
+          return renderPlayerBox(gameRole.characters[0], index);
+        })}
+      </Box>
     </Box>
   );
 };
