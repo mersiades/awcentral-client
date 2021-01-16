8d3122c437da647ca3bd0368ee21d2dbcc243f29
Michael Mersiades <fakename014@gmail.com>
2021-01-16 11:45:40 +1100

    Moves chevron on BarterBox

--- src/components/characterSheet/BarterBox.tsx
+++ src/components/characterSheet/BarterBox.tsx
@@ -30,16 +30,16 @@ const BarterBox: FC<BarterBoxProps> = ({ barter, instructions, handleSetBarter, 
     <Box fill="horizontal" align="center" justify="start">
       <Box fill="horizontal" direction="row" justify="between" align="center" pad={{ vertical: '12px' }}>
         <Box direction="row" align="center" gap="12px" pad={{ vertical: '12px' }}>
-          {showInstructions ? (
-            <FormUp onClick={() => setShowInstructions(false)} />
-          ) : (
-            <FormDown onClick={() => setShowInstructions(true)} />
-          )}
           <HeadingWS crustReady={crustReady} level="3" margin="0px">
             Barter
           </HeadingWS>
         </Box>
         <Box direction="row" align="center" gap="12px">
+          {showInstructions ? (
+            <FormUp onClick={() => setShowInstructions(false)} />
+          ) : (
+            <FormDown onClick={() => setShowInstructions(true)} />
+          )}
           <RedBox width="50px" align="center" margin={{ left: '12px' }}>
             <HeadingWS crustReady={crustReady} level="2" margin={{ left: '9px', right: '9px', bottom: '3px', top: '9px' }}>
               {barter}
