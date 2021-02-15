0820120e594f65f52b2254ab48e89ac39e195e2f
Michael Mersiades <fakename014@gmail.com>
2021-02-15 18:29:41 +1100

    Re-styles BattleVehicleForm to have horizontal display at the top

--- src/components/characterCreation/BattleVehicleForm.tsx
+++ src/components/characterCreation/BattleVehicleForm.tsx
@@ -453,119 +453,9 @@ const BattleVehicleForm: FC<BattleVehicleFormProps> = ({ navigateOnSet, existing
   }
 
   return (
-    <Box
-      data-testid="vehicle-form"
-      direction="column"
-      width="80vw"
-      align="start"
-      justify="start"
-      overflow="auto"
-      flex="grow"
-    >
-      <Box direction="row">
-        <Box direction="column" fill="horizontal" pad="12px" gap="12px">
-          <TextWS>{introText}</TextWS>
-          <Box>
-            <TextWS>
-              <strong>Give your vehicle a name</strong> (make/model, nickname, whatever):
-            </TextWS>
-            <TextInput
-              aria-label="name-input"
-              name="name"
-              value={name}
-              onChange={(e) => dispatch({ type: 'SET_NAME', payload: e.target.value })}
-            />
-          </Box>
-          <Box>
-            <TextWS>Choose its frame (resets other settings):</TextWS>
-            <Box direction="row" margin={{ top: '3px' }} wrap>
-              {carCreator.frames.map((frame) => renderFramePill(frame))}
-            </Box>
-          </Box>
-          <Box>
-            <TextWS>
-              <strong>Strengths</strong> (choose 1 or 2):
-            </TextWS>
-            <Box direction="row" margin={{ top: '3px' }} wrap>
-              {strengthOptions?.map((strength) => {
-                const isSelected = strengths.includes(strength);
-                return renderPill(strength, isSelected, () => handleOptionSelect(strength, 'strength', isSelected));
-              })}
-            </Box>
-          </Box>
-          <Box>
-            <TextWS>
-              <strong>Looks</strong> (choose 1 or 2):
-            </TextWS>
-            <Box direction="row" margin={{ top: '3px' }} wrap>
-              {lookOptions?.map((look) => {
-                const isSelected = looks.includes(look);
-                return renderPill(look, isSelected, () => handleOptionSelect(look, 'look', isSelected));
-              })}
-            </Box>
-          </Box>
-          <Box>
-            <TextWS>
-              <strong>Weaknesses</strong> (choose 1 or 2):
-            </TextWS>
-            <Box direction="row" margin={{ top: '3px' }} wrap>
-              {weaknessOptions?.map((weakness) => {
-                const isSelected = weaknesses.includes(weakness);
-                return renderPill(weakness, isSelected, () => handleOptionSelect(weakness, 'weakness', isSelected));
-              })}
-            </Box>
-          </Box>
-          <Box>
-            <TextWS>
-              <strong>Regular battle options</strong> (choose {vehicleFrame.battleOptionCount})
-            </TextWS>
-            <Box direction="row" margin={{ top: '3px' }} wrap>
-              {battleOptionOptions?.map((battleOption) => {
-                const isSelected = !!battleOptions.find((bo: VehicleBattleOption) => bo.id === battleOption.id);
-                return renderBattleOptionPill(battleOption, isSelected, () =>
-                  handleOptionSelect(battleOption, 'battleOption', isSelected)
-                );
-              })}
-            </Box>
-          </Box>
-          <Box>
-            <TextWS>
-              <strong>Battle Vehicle options</strong> (choose 2)
-            </TextWS>
-            <Box margin={{ top: '3px' }} gap="6px">
-              {battleVehicleOptionOptions?.map((battleOption) => {
-                const isSelected = !!battleVehicleOptions.find((bo: VehicleBattleOption) => bo.id === battleOption.id);
-                return (
-                  <CheckBox
-                    key={battleOption.id}
-                    checked={isSelected}
-                    label={battleOption.name}
-                    onChange={() => handleOptionSelect(battleOption, 'battleVehicleOption', isSelected)}
-                  />
-                );
-              })}
-            </Box>
-          </Box>
-        </Box>
-        <Box margin="12px" flex="grow" style={{ maxWidth: '200px' }} width="200px">
-          <Box fill="horizontal" align="center" justify="center">
-            <HeadingWS aria-label="vehicle-name" level={3} crustReady={crustReady} margin={{ vertical: '3px' }}>
-              {name}
-            </HeadingWS>
-            <DoubleRedBox value={vehicleFrame?.frameType} label="Frame" />
-          </Box>
-          <Box fill="horizontal" direction="row" align="center" justify="between">
-            <SingleRedBox value={speed} label="Speed" width="80px" />
-            <SingleRedBox value={handling} label="Handling" width="80px" />
-          </Box>
-          <Box fill="horizontal" direction="row" align="center" justify="between">
-            <SingleRedBox value={armor} label="Armor" width="80px" />
-            <SingleRedBox value={massive} label="Massive" width="80px" />
-          </Box>
-          {strengths.concat(looks).concat(weaknesses).length > 0 && (
-            <RedTagsBox tags={strengths.concat(looks).concat(weaknesses)} label="Tags" height="132px" />
-          )}
-          {weapons.length > 0 && <RedTagsBox tags={weapons} label="Weapons" height="132px" />}
+    <Box data-testid="battle-vehicle-form" direction="column" width="90vw" align="start" justify="start" flex="grow">
+      <Box direction="row" fill="horizontal" flex="grow" align="center" gap="12px" wrap>
+        <Box direction="row" gap="12px" align="center" height="90px" pad={{ bottom: '24px' }}>
           <ButtonWS
             primary
             alignSelf="center"
@@ -575,6 +465,107 @@ const BattleVehicleForm: FC<BattleVehicleFormProps> = ({ navigateOnSet, existing
             onClick={() => !settingVehicle && handleSetVehicle()}
             disabled={settingVehicle || battleOptions.length < vehicleFrame.battleOptionCount}
           />
+          <HeadingWS aria-label="vehicle-name" level={3} crustReady={crustReady} margin={{ top: '9px', bottom: '3px' }}>
+            {name}
+          </HeadingWS>
+        </Box>
+        <Box direction="row" wrap gap="6px">
+          <DoubleRedBox value={vehicleFrame?.frameType} label="Frame" />
+
+          <SingleRedBox value={speed} label="Speed" width="80px" />
+          <SingleRedBox value={handling} label="Handling" width="80px" />
+
+          <SingleRedBox value={armor} label="Armor" width="80px" />
+          <SingleRedBox value={massive} label="Massive" width="80px" />
+
+          {strengths.concat(looks).concat(weaknesses).length > 0 && (
+            <RedTagsBox tags={strengths.concat(looks).concat(weaknesses)} label="Tags" height="90px" maxWidth="350px" />
+          )}
+          {weapons.length > 0 && <RedTagsBox tags={weapons} label="Weapons" height="90px" maxWidth="650px" />}
+        </Box>
+      </Box>
+      <Box direction="column" fill="horizontal" pad="12px" gap="12px" overflow="auto" height="50vh">
+        <TextWS>{introText}</TextWS>
+        <Box flex="grow">
+          <TextWS>
+            <strong>Give your vehicle a name</strong> (make/model, nickname, whatever):
+          </TextWS>
+          <TextInput
+            aria-label="name-input"
+            name="name"
+            value={name}
+            onChange={(e) => dispatch({ type: 'SET_NAME', payload: e.target.value })}
+          />
+        </Box>
+        <Box flex="grow">
+          <TextWS>Choose its frame (resets other settings):</TextWS>
+          <Box direction="row" margin={{ top: '3px' }} wrap>
+            {carCreator.frames.map((frame) => renderFramePill(frame))}
+          </Box>
+        </Box>
+        <Box flex="grow">
+          <TextWS>
+            <strong>Strengths</strong> (choose 1 or 2):
+          </TextWS>
+          <Box direction="row" margin={{ top: '3px' }} wrap>
+            {strengthOptions?.map((strength) => {
+              const isSelected = strengths.includes(strength);
+              return renderPill(strength, isSelected, () => handleOptionSelect(strength, 'strength', isSelected));
+            })}
+          </Box>
+        </Box>
+        <Box flex="grow">
+          <TextWS>
+            <strong>Looks</strong> (choose 1 or 2):
+          </TextWS>
+          <Box direction="row" margin={{ top: '3px' }} wrap>
+            {lookOptions?.map((look) => {
+              const isSelected = looks.includes(look);
+              return renderPill(look, isSelected, () => handleOptionSelect(look, 'look', isSelected));
+            })}
+          </Box>
+        </Box>
+        <Box flex="grow">
+          <TextWS>
+            <strong>Weaknesses</strong> (choose 1 or 2):
+          </TextWS>
+          <Box direction="row" margin={{ top: '3px' }} wrap>
+            {weaknessOptions?.map((weakness) => {
+              const isSelected = weaknesses.includes(weakness);
+              return renderPill(weakness, isSelected, () => handleOptionSelect(weakness, 'weakness', isSelected));
+            })}
+          </Box>
+        </Box>
+        <Box flex="grow">
+          <TextWS>
+            <strong>Regular battle options</strong> (choose {vehicleFrame.battleOptionCount})
+          </TextWS>
+          <Box direction="row" margin={{ top: '3px' }} wrap>
+            {battleOptionOptions?.map((battleOption) => {
+              const isSelected = !!battleOptions.find((bo: VehicleBattleOption) => bo.id === battleOption.id);
+              return renderBattleOptionPill(battleOption, isSelected, () =>
+                handleOptionSelect(battleOption, 'battleOption', isSelected)
+              );
+            })}
+          </Box>
+        </Box>
+        <Box flex="grow">
+          <TextWS>
+            <strong>Battle Vehicle options</strong> (choose 2)
+          </TextWS>
+          <Box margin={{ top: '3px' }} gap="6px">
+            {battleVehicleOptionOptions?.map((battleOption) => {
+              const isSelected = !!battleVehicleOptions.find((bo: VehicleBattleOption) => bo.id === battleOption.id);
+              return (
+                <CheckBox
+                  key={battleOption.id}
+                  checked={isSelected}
+                  label={battleOption.name}
+                  onChange={() => handleOptionSelect(battleOption, 'battleVehicleOption', isSelected)}
+                />
+              );
+            })}
+          </Box>
         </Box>
       </Box>
     </Box>
